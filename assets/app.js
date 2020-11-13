e950772859c48b3f31ee13f73a6a9213d60af836
neiesc <edinei.esc@gmail.com>
2020-11-13 03:08:56 +0000

    Deploying to gh-pages from  @ d928d3894436b1c3142719bba2cb3082104a7ed7 🚀

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+(async () => {
+  const response = await fetch("../tmp/instagram.json");
+  const data = await response.json();
+
+  const htmlList = data
+    .map(({ src }) => `<li><img src="${src}"></li>`)
+    .join("");
+
+  document.querySelector("main").innerHTML = htmlList;
+})();
